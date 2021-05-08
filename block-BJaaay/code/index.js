d4cb3659bd181e113dc9cd6776b34f8aecf42e93
Aman Thakur <uttamthakur2999@gmail.com>
2021-05-08 21:16:04 +0530

    Using Loops

--- block-BJaaay/code/index.js
+++ block-BJaaay/code/index.js
@@ -3,11 +3,26 @@
 // Do the following using loops:
 
 // Using `console.log` log all the values from 1 to 10.
+for (let i=1 ; i <= 10 ; i++){
+    console.log(1);
+}
 
 // Using `console.log` log all the evan values from 1 to 10.
-
+for (let a=1 ; a <= 10 ; a++){
+    if (a% 2===0){
+        console.log(a);
+    }
+}
 // Using `console.log` log all the od values from 1 to 10.
-
+for (let x=1 ; x <= 10 ; x = 1+1){
+    if (x% 2 !==0){
+        console.log(x);
+    }
+}
 // Calculate the sum of all numbers from 1 to 10.
-
+for (let y=1 ; y<= 10 ; y = 1+1){
+    if (y+1){
+        console.log(y);
+    }
+}
 // Log all the values from 1 to 10 using while loop
