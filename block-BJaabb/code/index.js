255697f211c271aa7ea5382747b0543f49833b9f
Aman Thakur <uttamthakur2999@gmail.com>
2021-05-10 09:30:53 +0530

    Making Decision In Code

--- block-BJaabb/code/index.js
+++ block-BJaabb/code/index.js
@@ -6,22 +6,60 @@
 4. Now change the value of `user` to `John` and log the value of both `user` and `userName`.
 */
 
+let user= prompt (`Enter user name`);
+ alert(user);
+ let userName=user;
+ console.log(userName ,user);
+ let age =22 , isLearning =true ;
+user=`john`;
+console.log(user ,userName);
+
 /* 2.
 Use `prompt` to take two numbers from user and store it in variable `numA` and `numB`. Create a third variable called `sum`, using `+` operator add numA and numB , store it in varible `sum` and using alert display `sum` in browser.
 */
+let numA= prompt(`enter first number`)
+let numB= prompt(`enter second number`)
+let sum=(numA+numB)
+alert(sum)
 
 // Loops
 
 // 3. Complete the following code to make the output be 0 2 4 6 8 10?
 
-for (let j = 0; j <= 10; ____) console.log(j);
+for (let j = 0; j <= 10; ++j) {
+    if (j%2==0){
+       console.log(j); 
+    }
+}
+
 
 // 4. Print the odd numbers from 9 to 1 (both inclusive) using a for loop.
 
+for (let j = 9; j >= 1; --j) {
+    if (j%2!==0) 
+    {
+        console.log(j);
+    }
+    }
+
 // 5. Concat all the numbers from 5 to 0 (both inclusive) in descending order using a while loop. (543210)
+let j=5;
+while (j>=0) {
+    console.log(j);
+    j--;
+    
+}
 
 // 6. Concat all the even numbers from 10 to 0 (both inclusive) in descending order using a while loop. (1086420)
 
+let j=10;
+while (j>=0) {
+    if (j%2==0)
+    {console.log(j);
+    }
+    --j;
+    
+}
 // Comparisoin
 
 /* 7. Take two value using prompt and store them in variables `num1` and `num2`. Check whether they are equal or not.
@@ -36,3 +74,15 @@ Example:
 "hello", -21; // false
 
 */
+let num1=prompt(`Enter first value`)
+let num2=prompt(`Enter second value`)
+if (num1==num2){
+    alert(`true`)
+}
+else if(num1 !==num2) {
+    alert(false)
+}
+else {
+    alert(`Enter a valid value`)
+}
+
